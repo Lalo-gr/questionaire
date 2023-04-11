ded93524a9bd9dce8db781b9a79db0a1ff5c77e1
Lalo-gr <einajera741@gmail.com>
2023-04-11 12:15:03 -0500

    "Finsished Main App"

--- question/src/App.jsx
+++ question/src/App.jsx
@@ -5,26 +5,56 @@ import "./App.css";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
-  const [selectedOption, setSelectedQuestion] = useState(null);
+  const [selectedOption, setSelcteOption] = useState(null);
   const [finished, setFinished] = useState(false);
   const [score, setScore] = useState(0);
 
-  function handleOptionClick(clickedOption){
-    if(selectedOption) return;
+  function handleOptionClick(clickedOption) {
+    if (selectedOption) return;
 
-    if(clickedOption.id === currentQuestion.answerId){
-      setScore(score + 1)
+    if (clickedOption.id === currentQuestion.answerId) {
+      setScore(score + 1);
     }
 
-    setSelectedQuestion(clickedOption)
-
+    setSelcteOption(clickedOption);
   }
 
+  function handleNextQuestionClick() {
+    if (!selectedOption) return;
+
+    if (currentQuestion.id === questions[questions.length - 1].id) {
+      setFinished(true);
+    } else {
+      const indexOfCurrentQuestion = questions.findIndex((question) => {
+        return question.id === currentQuestion.id;
+      });
+      setCurrentQuestion(questions[indexOfCurrentQuestion + 1]);
+      setSelcteOption(null);
+    }
+  }
+
+  function handleRetryClick(){
+    setSelcteOption(null)
+    setCurrentQuestion(questions[0])
+    setScore(0)
+    setFinished(false)
+  }
 
   return (
     <div className="App">
       <div className="quiz">
-        <div className="info">
+        {finished ? (
+          <div className="end-screen">
+            <p className="finished">Finished!</p>
+            <p>You scored a {100 * (score/ questions.length)}%</p>
+            <button
+            className="retry-btn"
+            onClick={handleRetryClick}
+            >Retry</button>
+          </div>
+        ): (
+          <>
+          <div className="info">
           <p className="question-counter">
             Question {currentQuestion.id}
             <span className="total">/{questions.length}</span>
@@ -36,20 +66,32 @@ function App() {
             <button
               onClick={() => handleOptionClick(option)}
               disabled={selectedOption !== null}
-              className={""}
+              className={`
+              option ${
+              option.id === selectedOption?.id
+              ? selectedOption?.id === currentQuestion.answerId ?
+              'correct' : 'incorect'
+              : ""
+              }
+            `}
               key={i}
             >
               {option.text}
             </button>
           ))}
-          <button 
-          className="next"
-          disabled={!selectedOption}
-          onClick={()=>null}
+          <button
+            className="next"
+            disabled={!selectedOption}
+            onClick={handleNextQuestionClick}
           >
-            {currentQuestion.id === questions[questions.length-1].id ? 'Finish' : 'Next'}
+            {currentQuestion.id === questions[questions.length - 1].id
+              ? "Finish"
+              : "Next"}
           </button>
         </div>
+          </>
+        )}
+        
       </div>
     </div>
   );
