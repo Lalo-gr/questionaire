8b8c06648dde1212d7777e4c929a50527d3d369c
Lalo-gr <einajera741@gmail.com>
2023-04-10 23:47:24 -0500

    "Adding the questions and buttons"

--- question/src/App.jsx
+++ question/src/App.jsx
@@ -1,12 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { questions } from "./data";
 
-import './App.css'
+import "./App.css";
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const [selectedOption, setSelectedQuestion] = useState(null);
+  const [finished, setFinished] = useState(false);
+  const [score, setScore] = useState(0);
+
   
+
   return (
-    null
-  )
+    <div className="App">
+      <div className="quiz">
+        <div className="info">
+          <p className="question-counter">
+            Question {currentQuestion.id}
+            <span className="total">{questions.length}</span>
+          </p>
+          <p className="question">{currentQuestion.question}</p>
+        </div>
+        <div className="options">
+          {currentQuestion.options.map((option, i) => (
+            <button
+              onClick={() => null}
+              disabled={selectedOption !== null}
+              className={""}
+              key={i}
+            >
+              {option.text}
+            </button>
+          ))}
+          <button 
+          className="next"
+          disabled={!selectedOption}
+          onClick={()=>null}
+          >
+            {currentQuestion.id === question[questions.length-1].id ? 'Finish' : 'Next'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
